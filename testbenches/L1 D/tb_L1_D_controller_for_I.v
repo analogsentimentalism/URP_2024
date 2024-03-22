4989926315e58a350e5fc77e5e1f97472ee5e927
analogsentimentalism <cjdemforhs@g.skku.edu>
2024-03-23 02:02:50 +0900

    [Edit] Testbench for L1_I_controller, add while(stall)

--- testbenches/L1 D/tb_L1_D_controller_for_I.v
+++ testbenches/L1 D/tb_L1_D_controller_for_I.v
@@ -9,6 +9,7 @@ reg					read_C_L1;
 reg					write_C_L1;
 reg					flush;
 reg					ready_L2_L1;
+reg					write_L1_L2_done;
 
 wire				stall;
 wire				refill;
@@ -26,9 +27,11 @@ L1_I_controller u_L1_D_controller (
     .flush				(flush),
     .ready_L2_L1		(ready_L2_L1),
     .write_C_L1			(write_C_L1),
+	.write_L1_L2_done	(write_L1_L2_done),
 
     .stall				(stall),
 	.refill				(refill),
+	.update				(update),
     .read_L1_L2			(read_L1_L2),
     .write_L1_L2		(write_L1_L2)
 );
@@ -48,146 +51,171 @@ initial begin: init
 	test_state	=	3'd0;
 
 	clk			=	1'b1;
-	nrst		<=	1'b0;
-	address		<=	64'b0;
-	read_C_L1	<=	1'b0;
-	write_C_L1	<=	1'b0;
-	flush		<=	1'b0;
-	ready_L2_L1	<=	1'b0;
+	nrst		=	1'b0;
+	address		=	64'b0;
+	read_C_L1	=	1'b0;
+	write_C_L1	=	1'b0;
+	flush		=	1'b0;
+	ready_L2_L1	=	1'b0;
 
-	for(i = 0; i<1000; i = i + 1) begin
-		address_array[i]	<=	$urandom & 32'hFFFF_FFFC;
+	for(i = 0; i<30; i = i + 1) begin
+		address_array[i]	=	$urandom & 32'hFFFF_F03C | {i[5:0], 6'b000000};
+		$display("%2d\t:\t%8h", i, address_array[i]);
+	end
+	for(i = 0; i<30; i = i + 1) begin
+		address_array[i + 30]	=	$urandom & 32'hFFFF_F03C | {address_array[i][11:6], 6'b000000};
+		$display("%2d\t:\t%8h", i + 30, address_array[i + 30]);
 	end
 end
 
 initial begin: test
 
-	// 0. Cache Init
-	$display("Init start");
+	// 0. Cache Init (Write)
+	$display("Cache Init (Write) start");
 
-	nrst		<=	1'b1;
-#10	nrst		<=	1'b0;
+	nrst		=	1'b1;
+#10	nrst		=	1'b0;
 
-#10	nrst		<=	1'b1;
-	read_C_L1	<=	1'b1;
+#10	nrst		=	1'b1;
+	write_C_L1	=	1'b1;	// Initial reset
 
 	for(i = 0; i<10; i = i + 1) begin
-		address		<= address_array[i];
+		address		= address_array[i];
 
 		$display("%4d: Address %h", i, address);
 
-	#8	ready_L2_L1	<=	1'b1;
-	#2	ready_L2_L1	<=	1'b0;
-	#4;
+	#8	ready_L2_L1	=	1'b1;
+	#2	ready_L2_L1	=	1'b0;
+		
+		while(stall) #2;
 	end
 
-	read_C_L1	<=	1'b0;
+	write_C_L1	=	1'b0;
 #100;
 
-	// 1. Data Read
-
-	// 1-1. Hit
-	$display("%4d: Read, Hit", $time);
+	// 1. Read: Hit
+	$display("%4d: Read-Hit", $time);
 	test_state	=	3'd1;
 
-	read_C_L1	<=	1'b1;
+	read_C_L1	=	1'b1;
 
 	for(i = 0; i<10; i = i + 1) begin
-		address		<=	address_array[i];
+		address		=	address_array[i];
 
 		$display("%4d: Address %h", $time, address);
-	#10;
+	#2;
+		while(stall) #2;
 	end
 
-	read_C_L1	<=	1'b0;
+	read_C_L1	=	1'b0;
 #100;
 
-	// 1-2. Miss - (Memory) Hit
-	$display("%4d: Read, Miss - Hit", $time);
+	// 2. Read: Miss - (Memory) Hit
+	$display("%4d: Read-Miss-Hit", $time);
 	test_state	=	3'd2;
 
-	read_C_L1	<=	1'b1;
+	read_C_L1	=	1'b1;
 
 	for(i = 10; i<20; i = i + 1) begin
-		address		<=	address_array[i];
+		address		=	address_array[i];
 
 		$display("%4d: Address %h", $time, address);
 	#8	
-		ready_L2_L1	<=	1'b1;
+		ready_L2_L1	=	1'b1;
 	#2
-		ready_L2_L1	<=	1'b0;
-	#4;
+		ready_L2_L1	=	1'b0;
+	#2;
+		while(stall) #2;
 	end
 
-	read_C_L1	<=	1'b0;
+	read_C_L1	=	1'b0;
 #100;
 
-	// 1-3. Miss - (Memory) Miss
-	$display("%4d: Read, Miss - Miss", $time);
+	// 3. Read: Miss - (Memory) Miss
+	$display("%4d: Read-Miss-Miss", $time);
 	test_state	=	3'd3;
 
-	read_C_L1	<=	1'b1;
+	read_C_L1	=	1'b1;
 
 	for(i = 20; i<30; i = i + 1) begin
-		address		<=	address_array[i];
+		address		=	address_array[i];
 
 		$display("%4d: Address %h", $time, address);
 	#20
-		ready_L2_L1	<=	1'b1;
+		ready_L2_L1	=	1'b1;
 	#2
-		ready_L2_L1	<=	1'b0;
-	#8;
+		ready_L2_L1	=	1'b0;
+	#2;
+		while(stall) #2;
 	end
 
-	read_C_L1	<=	1'b0;
+	read_C_L1	=	1'b0;
 #100;
 
-	// 2. Data Write
-
-	// 2-1. Not Changed
+	// 4. Cache Replacement: Hit
+	$display("%4d: Cache Replacement-Hit", $time);
 	test_state	=	3'd4;
 
-	// 2-2. Write back
+	read_C_L1	=	1'b1;
+
+
+	for(i = 30; i<60; i = i + 1) begin
+		address		=	address_array[i];
+
+		$display("%4d: Address %h", $time, address);
+	#20
+		ready_L2_L1	=	1'b1;
+	#2
+		ready_L2_L1	=	1'b0;
+	#2;
+		while(stall) #2;
+	end
+	
+#100;
+
+	// 5. Cache Replacement: Miss
+	$display("%4d: Cache Replacement-Miss", $time);
 	test_state	=	3'd5;
 
-	// 3. Flush
+	write_C_L1	=	1'b1;
+
+	for(i = 30; i<40; i = i + 1) begin
+		address		=	address_array[i];
+
+		$display("%4d: Address %h", $time, address);
+	#20
+		ready_L2_L1	=	1'b1;
+	#2
+		ready_L2_L1	=	1'b0;
+	#2;
+		while(stall) #2;
+	end
+
+	write_C_L1	=	1'b0;
+
+	read_C_L1	=1'b1;
+
+	for(i = 0; i<30; i = i + 1) begin
+		address		=	address_array[i];
+	#2;
+		while(stall) #2;
+	end
+
+#100;
+
+	// 6. Flush
 	$display("%4d: FLUSH", $time);
 	test_state	=	3'd6;
 
-	flush	<=	1'b1;
+	flush	=	1'b1;
 
 #100;
-
-	// (4. Reset) -> deleted
-	// 	$display("%4d: FLUSH", $time);
-	// 	nrst	= 1'b0;
-
-	// #100;
-
-	// 4. Cache Replacement
-	$display("%4d: Cache Replacement", $time);
-	test_state	=	3'd7;
-
-	nrst		<=	1'b1;
-	flush		<=	1'b0;
-	read_C_L1	<=	1'b1;
-
-	for(i = 0; i<100; i = i + 1) begin
-		address		<=	address_array[i];
-
-		$display("%4d: Address %h", i, address);
-
-	#2	ready_L2_L1	<=	1'b1;
-	#2	ready_L2_L1	<=	1'b0;
-	#6;
-	end
-
 	$stop;
 end
 
 initial begin
+	$dumpfile("tb_L1_D_controller_for_I.vcd");
 	$dumpvars(u_L1_D_controller);
-	$dumpfile("dump.vcd");
 end
 
 
