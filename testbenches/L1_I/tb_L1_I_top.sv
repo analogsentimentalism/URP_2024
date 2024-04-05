27a77eea59a38dcd3af8b21472641efca7d5928c
analogsentimentalism <cjdemforhs@g.skku.edu>
2024-04-05 16:25:58 +0900

    [Add] tb_L1_I_top, L1_I_top changed.

--- testbenches/L1_I/tb_L1_I_top.sv
+++ testbenches/L1_I/tb_L1_I_top.sv
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-module tb_L1_I_controller #(
+module tb_L1_I_top #(
 	parameter	L1_CLK	= 1,			// L1의 클락
 	parameter	L2_CLK	= 1,			// L2의 클락
 	parameter	TOTAL	= 64,			// 전체 address 개수
@@ -12,42 +12,48 @@ reg						clk				;
 reg						nrst			;
 
 reg		[31:0]			address			; 
-reg						read_C_L1		;
 reg						flush			;
 reg						ready_L2_L1		;
+reg						read_C_L1		;
+
+reg		[511:0]			read_data_L2_L1	;
+wire	[31:0]			read_data_L1_C	;
 
 wire					stall			;
-wire					refill			;
 wire					read_L1_L2		;
-wire					way				;
 wire	[INUM - 1:0]	index_L1_L2		;
 wire	[TNUM - 1:0]	tag_L1_L2		;
 
-L1_I_controller u_L1_I_controller (
-
+L1_I_top u_L1_I_top (
 	.clk			(	clk					),
     .nrst			(	nrst				),
 
     .tag_C_L1		(	address[31-:TNUM]	),
     .index_C_L1		(	address[6+:INUM]	),
-    .read_C_L1		(	read_C_L1			),
+	.offset			(	address[5:0]		),
     .flush			(	flush				),
-    .ready_L2_L1	(	ready_L2_L1			),
-
     .stall			(	stall				),
-	.refill			(	refill				),
-	.way			(	way					),
+
+	.read_data_L1_C	(	read_data_L1_C		),
+	.read_data_L2_L1(	read_data_L2_L1		),
+
     .read_L1_L2		(	read_L1_L2			),
+    .ready_L2_L1	(	ready_L2_L1			),
+    .read_C_L1		(	read_C_L1			),
    
 	.index_L1_L2	(	index_L1_L2			),
 	.tag_L1_L2		(	tag_L1_L2			)
 );
 
+
 // Signals for Testbench
 reg		[31:0]	address_array	[TOTAL-1:0]	;
 reg		[31:0]	replace_array	[TOTAL-1:0]	;
+reg		[511:0]	data_array		[TOTAL-1:0]	;
+reg		[511:0]	rdata_array		[TOTAL-1:0]	;
+
 integer			i, j						;
-integer			aa, ra						;
+integer			aa, ra, d, r				;
 
 integer			test_state					;
 
@@ -64,58 +70,71 @@ initial begin: init
 	flush		= '0	;
 	ready_L2_L1	= '0	;
 
-	aa	= $fopen("../testbenches/etc/tb_L1_I_controller_address_array.txt", "wb");
-	ra	= $fopen("../testbenches/etc/tb_L1_I_controller_replace_array.txt", "wb");
+	aa	= $fopen("../testbenches/etc/tb_L1_I_top_address_array.txt", "wb");
+	ra	= $fopen("../testbenches/etc/tb_L1_I_top_replace_array.txt", "wb");
+	d	= $fopen("../testbenches/etc/tb_L1_I_top_data_array.txt", "wb");
+	r	= $fopen("../testbenches/etc/tb_L1_I_top_rdata_array.txt", "wb");
 
 	@(posedge clk);	// 파일 오픈 적용이 잘 안될까봐
 
 	for(i = 0; i<TOTAL; i = i + 1) begin   // random addresses
-		address_array[i]	= $urandom & 32'hFFFF_F03C | {i[0+:INUM], 6'd0};
-		$fwrite(aa, "%h\n", address_array[i])									;
+		address_array[i]	= $urandom & 32'hFFFF_F03C | {i[0+:INUM], 6'd0}	;
+		$fwrite(aa, "%h\n", address_array[i])								;
 	end
 	for(i = 0; i<TOTAL; i = i + 1) begin   // random addresses with same index
 		replace_array[i]	= $urandom & 32'hFFFF_F03C | {address_array[i][6+:INUM], 6'd0}	;
-		$fwriteh(ra, "%h\n",replace_array[i])													;
+		$fwriteh(ra, "%h\n",replace_array[i])												;
+	end
+	for(i = 0; i<TOTAL; i = i + 1) begin   // random addresses
+		data_array[i]	= $urandom			;
+		$fwrite(d, "%h\n", data_array[i])	;
+	end
+	for(i = 0; i<TOTAL; i = i + 1) begin   // random addresses with same index
+		rdata_array[i]	= $urandom			;
+		$fwriteh(r, "%h\n",rdata_array[i])	;
 	end
 
-	$fclose(aa);
-	$fclose(ra);
+	$fclose(aa)	;
+	$fclose(ra)	;
+	$fclose(d)	;
+	$fclose(r)	;
 end
 
 initial begin: test
 
 	// 1. Read: L1 Miss - L2 Hit way0.
-	$display("%6d: Cache Init start - way0", $time)		;
-	test_state	= 1							;
+	$display("%6d: Cache Init start - way0", $time)	;
+	test_state	= 1									;
 
-	nrst		= 1'b1						;
-	repeat(5 * L1_CLK)	@(posedge	clk)	;
-	nrst		= 1'b0						;
+	nrst		= 1'b1								;
+	repeat(5 * L1_CLK)	@(posedge	clk)			;
+	nrst		= 1'b0								;
 
-	repeat(5 * L1_CLK)	@(posedge	clk)	;
-	nrst		= 1'b1						;
-	read_C_L1	= 1'b1						;   // Initial reset
+	repeat(5 * L1_CLK)	@(posedge	clk)			;
+	nrst		= 1'b1								;
+	read_C_L1	= 1'b1								;   // Initial reset
 
 	for(i = 0; i<INIT; i = i + 1) begin	// fill way0.
-		address		= address_array[i]				;
+		address		= address_array[i]					;
 
 		$display("%6d: Read Address %h", $time, address);
 
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(2 * L2_CLK)  @(posedge   clk)			;
-		ready_L2_L1	= 1'b1								;
+		ready_L2_L1		= 1'b1							;
+		read_data_L2_L1	= data_array[i]					;
 		repeat(L2_CLK)	@(posedge   clk)				;
-		ready_L2_L1	= 1'b0								;
+		ready_L2_L1		= 1'b0							;
       
 		while(stall)	repeat(L1_CLK)	@(posedge   clk);
 	end
 
-	repeat(50)	@(posedge   clk)		;	// test_state 바뀔 때 구분.
-											// Cache clock과 관련 X
+	repeat(50)	@(posedge   clk);	// test_state 바뀔 때 구분.
+									// Cache clock과 관련 X
 
 	// 2. Read: L1 Miss - L2 Hit way1.
 	$display("Cache Init start - way1")	;
-	test_state	= 2	;
+	test_state	= 2						;
 
 	for(i = INIT; i<TOTAL; i = i + 1) begin	// fill way1.
 		address		= address_array[i]					;
@@ -124,9 +143,10 @@ initial begin: test
 
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(2 * L2_CLK)   @(posedge   clk)			;
-		ready_L2_L1	= 1'b1								;
+		ready_L2_L1		= 1'b1							;
+		read_data_L2_L1	= data_array[i]					;
 		repeat(L2_CLK)	@(posedge   clk)				;
-		ready_L2_L1	= 1'b0								;
+		ready_L2_L1		= 1'b0							;
       
 		while(stall)	repeat(L1_CLK)	@(posedge   clk);
 	end
@@ -148,11 +168,11 @@ initial begin: test
 		while(stall)	repeat(L1_CLK)	@(posedge   clk);
 	end
 
-	repeat(50)	@(posedge   clk)	;
+	repeat(50)	@(posedge   clk);
 
 	// 4. Read: L1 Hit way1.
-	$display("%6d: Read-Hit-way1", $time);
-	test_state	= 4	;
+	$display("%6d: Read-Hit-way1", $time)	;
+	test_state	= 4							;
 
 	for(i = INIT; i<TOTAL; i = i + 1) begin	// read hit way 1
 		address	= address_array[i]						;
@@ -167,9 +187,9 @@ initial begin: test
 
 	// 5. Read: L1 Miss - L2 Hit (Replace way0)
 	$display("%6d: Read-Miss-Hit (Replace way0)", $time);
-	test_state	= 5							;
+	test_state	= 5										;
 
-	read_C_L1	= 1'b1						;
+	read_C_L1	= 1'b1									;
 
 	for(i = 0; i<INIT; i = i + 1) begin
 		address			= replace_array[i]				;
@@ -178,9 +198,10 @@ initial begin: test
 
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(2 * L2_CLK)   @(posedge   clk)			;
-		ready_L2_L1	= 1'b1								;
+		ready_L2_L1		= 1'b1							;
+		read_data_L2_L1	= rdata_array[i]				;
 		repeat(L2_CLK)	@(posedge   clk)				;
-		ready_L2_L1	= 1'b0								;
+		ready_L2_L1		= 1'b0							;
 		repeat(L1_CLK)	@(posedge   clk)				;
 		while(stall)	repeat(L1_CLK)	@(posedge   clk);
 	end
@@ -196,6 +217,7 @@ initial begin: test
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(2 * L2_CLK)   @(posedge   clk)			;		
 		ready_L2_L1		= 1'b1							;
+		read_data_L2_L1	= rdata_array[i]				;
 		repeat(L2_CLK)   @(posedge   clk)				;
 		ready_L2_L1		= 1'b0							;
 		repeat(L1_CLK)	@(posedge   clk)				;
@@ -218,28 +240,29 @@ initial begin: test
 	// 8. Read: L1 Miss - L2 Miss way0.
 	$display("%6d: Cache Init start - way0", $time)	;
 	test_state	= 8									;
-
+	
 	read_C_L1	= 1'b1								;
 
 	for(i = 0; i<INIT; i = i + 1) begin	// fill way0.
-		address		= address_array[i]					;
+		address		= address_array[i]				;
 
 		$display("%6d: Read Address %h", $time, address);
 
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(4 * L2_CLK + 8)  @(posedge   clk)		;
 		ready_L2_L1	= 1'b1								;
+		read_data_L2_L1	= data_array[i]					;
 		repeat(L2_CLK)	@(posedge   clk)				;
 		ready_L2_L1	= 1'b0								;
       
 		while(stall)	repeat(L1_CLK)	@(posedge   clk);
 	end
 
-	repeat(50)	@(posedge   clk)		;
+	repeat(50)	@(posedge   clk)	;
 
 	// 9. Read: L1 Miss - L2 Hit way1.
 	$display("Cache Init start - way1")	;
-	test_state	= 9	;
+	test_state	= 9						;
 
 	for(i = INIT; i<TOTAL; i = i + 1) begin	// fill way1.
 		address		= address_array[i]					;
@@ -249,6 +272,7 @@ initial begin: test
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(4 * L2_CLK + 8)  @(posedge   clk)		;
 		ready_L2_L1	= 1'b1								;
+		read_data_L2_L1	= data_array[i]					;
 		repeat(L2_CLK)	@(posedge   clk)				;
 		ready_L2_L1	= 1'b0								;
       
@@ -260,8 +284,8 @@ initial begin: test
 
 	// 10. Read: L1 Miss - L2 Miss (Replace way0)
 	$display("%6d: Read-Miss-Miss (Replace way0)", $time)	;
-	test_state	= 10										;
-	read_C_L1	= 1'b1										;
+	test_state	= 10						;
+	read_C_L1	= 1'b1						;
 
 	for(i = 0; i<INIT; i = i + 1) begin
 		address			= replace_array[i]				;
@@ -271,6 +295,7 @@ initial begin: test
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(4 * L2_CLK + 8)  @(posedge   clk)		;
 		ready_L2_L1	= 1'b1								;
+		read_data_L2_L1	= rdata_array[i]				;
 		repeat(L2_CLK)	@(posedge   clk)				;
 		ready_L2_L1	= 1'b0								;
 		repeat(L1_CLK)	@(posedge   clk)				;
@@ -288,6 +313,7 @@ initial begin: test
 		repeat(2 * L1_CLK)	@(posedge   clk)			;
 		repeat(2 * L2_CLK)   @(posedge   clk)			;		
 		ready_L2_L1		= 1'b1							;
+		read_data_L2_L1	= rdata_array[i]				;
 		repeat(L2_CLK)   @(posedge   clk)				;
 		ready_L2_L1		= 1'b0							;
 		repeat(L1_CLK)	@(posedge   clk)				;
@@ -298,8 +324,8 @@ initial begin: test
 end
 
 initial begin
-	$dumpfile("tb_L1_I_controller.vcd")	;
-	$dumpvars(u_L1_I_controller)		;
+	$dumpfile("tb_L1_I_top.vcd");
+	$dumpvars(u_L1_I_top)		;
 end
 
 endmodule
