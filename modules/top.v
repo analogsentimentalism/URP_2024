5a45fbf908940c2a1a86416ef14a3dfdf4c1c6f7
kang0029 <rkdtnalsdd@naver.com>
2024-05-01 13:58:27 +0900

    [Edit]

--- modules/top.v
+++ modules/top.v
@@ -43,7 +43,6 @@ wire [17:0] tag_L1D_L2, write_tag_L1D_L2, tag_L1I_L2;
 wire [17:0] tag_L1_L2;
 wire [7:0] index_L1D_L2, index_L1I_L2, write_index_L1D_L2;
 wire [7:0] index_L1_L2;
-wire [31:0] read_data_L1D_C, read_data_L1I_C;
 
 
 
