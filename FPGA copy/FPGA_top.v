f56efaceaef8cab6aa38e6de55941c45bc956587
analogsentimentalism <cjdemforhs@g.skku.edu>
2024-05-31 21:07:52 +0900

    [Edit] WIP new cpu

--- FPGA copy/FPGA_top.v
+++ FPGA copy/FPGA_top.v
@@ -14,42 +14,80 @@ module FPGA_top #(
 	output	[3:0]	test_led
 );
 
-wire	L2_miss;
-wire	L1I_miss;
-wire	L1D_miss;
-wire	read_C_L1I, read_C_L1D, write_C_L1D, read_L1_L2, write_L1_L2;
-wire 	[7:0] data_o;
-wire 	done;
-wire	tx_data_wire			;
+wire	[31:0]	rw_address;
+wire	[31:0]	read_data;
+wire			read_request;
+wire			read_response;
+wire	[31:0]	write_data;
+wire	[3:0]	write_strobe;
+wire			write_request;
+wire			write_response;
 
-assign	tx_data = tx_data_wire	;
+rvsteel_core #(
+  .BOOT_ADDRESS	(	PC_START	)
+  ) (
 
-assign	test_led[0]	= done		;
-assign	test_led[1]	= tx_data_wire	;
-assign	test_led[2]	= data_o[1]	;
-assign	test_led[3]	= data_o[0]	;
+  // Global signals
 
-riscV32I #(
-	.RAM_WIDTH			(	RAM_WIDTH			),
-	.RAM_DEPTH			(	RAM_DEPTH			),
-	.RAM_PERFORMANCE 	(	RAM_PERFORMANCE		),
-	.INIT_FILE			(	INIT_FILE			),
-	.START_ADDR			(	START_ADDR			),
-	.PC_START			(	PC_START			)
-) u_cpu (
-	.clk(clk),
-	.clk_mem(clk_mem),
-	.rst(rst),
-	.enb(enb),
-	.L2_miss_o(L2_miss),
-	.L1I_miss_o(L1I_miss),
-	.L1D_miss_o(L1D_miss),
-	.read_C_L1I_o(read_C_L1I),
-	.read_C_L1D_o(read_C_L1D),
-	.write_C_L1D_o(write_C_L1D),
-	.read_L1_L2(read_L1_L2),
-	.write_L1_L2(write_L1_L2)
+	.clock	(	clk		),
+	.reset	(	rst		),
+	.halt	(	1'b0	),
+
+	// IO interface
+
+	.rw_address		(	rw_address		),
+	.read_data		(	read_data		),
+	.read_request	(	read_request	),
+	.read_response	(	read_response	),
+	.write_data		(	write_data		),
+	.write_strobe	(	wite_strobe		),
+	.write_request	(	write_request	),
+	.write_response	(	write_response	),
+
+	// Interrupt signals (hardwire inputs to zero if unused)
+
+	.irq_external			(	1'b0	),
+	.irq_external_response	(			),
+	.irq_timer				(	1'b0	),
+	.irq_timer_response		(			),
+	.irq_software			(	1'b0	),
+	.irq_software_response	(			),
+	.irq_fast				(	1'b0	),
+	.irq_fast_response		(			),
+
+	// Real Time Clock (hardwire to zero if unused)
+
+	.real_time_clock	(	64'b0	)
+
+  );
+
+top	u_top (
+
 );
+
+
+
+// riscV32I #(
+// 	.RAM_WIDTH			(	RAM_WIDTH			),
+// 	.RAM_DEPTH			(	RAM_DEPTH			),
+// 	.RAM_PERFORMANCE 	(	RAM_PERFORMANCE		),
+// 	.INIT_FILE			(	INIT_FILE			),
+// 	.START_ADDR			(	START_ADDR			),
+// 	.PC_START			(	PC_START			)
+// ) u_cpu (
+// 	.clk(clk),
+// 	.clk_mem(clk_mem),
+// 	.rst(rst),
+// 	.enb(enb),
+// 	.L2_miss_o(L2_miss),
+// 	.L1I_miss_o(L1I_miss),
+// 	.L1D_miss_o(L1D_miss),
+// 	.read_C_L1I_o(read_C_L1I),
+// 	.read_C_L1D_o(read_C_L1D),
+// 	.write_C_L1D_o(write_C_L1D),
+// 	.read_L1_L2(read_L1_L2),
+// 	.write_L1_L2(write_L1_L2)
+// );
 
 counter u_counter (
 	.clk(clk_mem),
