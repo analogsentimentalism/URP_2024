944971616e1778fad2e38ffa8f0dbf85e02c93c8
kang0029 <rkdtnalsdd@naver.com>
2024-06-05 12:50:46 +0900

    [edit]

--- FPGA copy/FPGA_top.v
+++ FPGA copy/FPGA_top.v
@@ -147,7 +147,14 @@ always @(posedge clk_cpu) begin
 		read_C_L1D_n	<= read_C_L1D;
 	end
 end
-
+LED_module u_LED_module (
+	.clk(clk_cpu),
+	.rstn(~rst),
+	.LED(LED),
+	.ready_L1I_C(ready_L1I_C),
+	.read_data_L1I_C(read_data_L1I_C),
+	.read_C_L1I(read_C_L1I)
+);
 top u_top (
 	.clk				(	clk_cpu					),
 	.nrst				(	~rst				),
@@ -251,6 +258,8 @@ counter u_counter (
 	.data_o(data_o),
 	.done(done)
 );
+
+
 
 uart_tx u_tx (
 	.rstn(~rst),
---
+++ FPGA copy/LED_module.v
@@ -0,0 +1,29 @@
+module LED_module(
+    input clk,
+    input rstn,
+    output [15:0] LED,
+    input ready_L1I_C,
+    input [31:0] read_data_L1_C,
+    input read_C_L1I
+);
+
+reg enable;
+
+always @(posedge clk or negedge rstn)
+begin
+    if(~rstn)
+        enable <= 1'b1;
+    else if (ready_L1I_C & read_C_L1I)
+        enable <= 1'b0;
+    else    
+        enable <= enable;
+end
+always @(posedge clk or negedge rstn)
+begin
+    if(~rstn)
+        LED <= 16'h0;
+    else if(ready_L1I_C & read_C_L1I & enable)
+        LED <= read_data_L1_C[15:0];
+    else
+        LED <= LED;
+end
